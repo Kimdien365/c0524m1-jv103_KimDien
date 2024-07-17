da43af1dba2b84c8a6e8558803bef7461467bc9c
Kimdien3656 <kimdien.ks@gmail.com>
2024-07-17 22:18:23 +0700

    up

---
+++ ss20_case_study/case_stydy_quan_ly_hoc_vien/Students.js
@@ -0,0 +1,10 @@
+class Students {
+    constructor(id, name, birthday, gender, classroom, image) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.classroom = classroom;
+        this.image = image;
+    }
+}
