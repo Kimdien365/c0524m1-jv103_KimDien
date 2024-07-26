42343b1c633f909a4e360a064463defca0adb430
Kimdien3656 <kimdien.ks@gmail.com>
2024-07-26 21:23:17 +0700

    nop_bai

---
+++ ss21_Exam/Mobile.js
@@ -0,0 +1,8 @@
+class Mobile {
+    constructor(id, name, manufacturer, price) {
+        this.id = id;
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.price = price;
+    }
+}
